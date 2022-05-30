94ab1d536073d94c3e93ea2ab507747d30667778
Rimas Kudelis <rimas.kudelis@adeoweb.biz>
2022-05-30 21:41:50 +0300

    Refactor processor code

--- src/Processor/AbstractProcessor.php
+++ src/Processor/AbstractProcessor.php
@@ -1,30 +1,53 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Egeniq\Monolog\Gdpr\Processor;
 
 use Monolog\Processor\ProcessorInterface;
 
 abstract class AbstractProcessor implements ProcessorInterface
 {
-    /**
-     * @var null|string
-     */
-    private $salt;
+    private string $salt = '';
 
-    /**
-     * @param string $salt
-     */
     public function setSalt(string $salt): void
     {
         $this->salt = $salt;
     }
 
     /**
-     * @param string $value
-     *
-     * @return string
+     * NOTE: the signature of this method will change when support for Monolog 3 is added.
      */
-    protected function getHashedValue(string $value)
+    public function __invoke(array $record): array
+    {
+        return $this->redactValuesRecursively($record);
+    }
+
+    private function redactValuesRecursively(array $record): array
+    {
+        foreach ($record as $key => $value) {
+            if (is_string($value)) {
+                $record[$key] = $this->redactStringValue($value);
+            } elseif (is_array($value)) {
+                $record[$key] = $this->redactValuesRecursively($value);
+            }
+        }
+
+        return $record;
+    }
+
+    private function redactStringValue(string $record): string
+    {
+        return preg_replace_callback(
+            static::REGEX_PATTERN,
+            function (array $matches): string {
+                return $this->getHashedValue($matches[0]);
+            },
+            $record
+        ) ?? $record;
+    }
+
+    private function getHashedValue(string $value): string
     {
         return sha1($value . $this->salt);
     }
--- src/Processor/RedactIpProcessor.php
+++ src/Processor/RedactIpProcessor.php
@@ -1,31 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Egeniq\Monolog\Gdpr\Processor;
 
 class RedactIpProcessor extends AbstractProcessor
 {
-    /**
-     * @param array $record
-     *
-     * @return array
-     */
-    public function __invoke(array $record): array
-    {
-        // serialise to a JSON string so we can scan the entire tree
-        $serialised = json_encode($record);
-
-        $filtered = preg_replace_callback(
-            "/(\b\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}\b)/",
-            function($matches) {
-                return $this->getHashedValue($matches[0]);
-            },
-            $serialised
-        );
-
-        if ($filtered) {
-            return json_decode($filtered, true);
-        }
-
-        return $record;
-    }
+    protected const REGEX_PATTERN = "/(\b\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}\b)/";
 }
